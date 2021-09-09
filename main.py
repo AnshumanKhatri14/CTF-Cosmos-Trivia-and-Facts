b6c538c6f40713761b6ebe0b7ba687722de9dd2e
Vishesh <sharmavishesh022@gmail.com>
2021-09-09 15:42:43 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# Trivia game using python and mysql connectivity (probably lol)
+
+=======
+# Trivia game using python and mysql connectivity (probably lol)
+
+>>>>>>> 3142c38e44eb369f5b8b7f4c28a31b51e8a917d5
