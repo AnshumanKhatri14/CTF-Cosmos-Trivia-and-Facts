2a7ce6e9b9bc0b485b3aae3b6f7563dc263998d0
Yugam4254 <yugamsehgal4254@gmail.com>
2021-09-09 22:42:18 +0530

    Initial Commit

--- Questions/question.py
+++ Questions/question.py
@@ -2,9 +2,10 @@ ques={   1:"Which is the smallest planet within our solar system?"
         ,2:"Which is the second smallest planet within our solar system?"
         ,3:"The moon called Titan orbits which planet?"
         ,4:"Which is the brightest planet in the night’s sky?"
-        ,5:"Which planet is larger, Neptune or Saturn?"
+        ,5:"Which is the largest planet within our solar system?"
         ,6:"Uranus has only been visited by what spacecraft?"
-        ,7:""
+}
+'''        ,7:""
         ,8:""
         ,9:""
         ,10:""
@@ -98,4 +99,4 @@ ques={   1:"Which is the smallest planet within our solar system?"
         ,98:""
         ,99:""
         ,100:""
-}
+}'''
