868d11d2f3a581957a37008071e33a622b43e70e
Yugam4254 <yugamsehgal4254@gmail.com>
2021-09-11 22:36:08 +0530

    Converted .txt Format To .csv

--- funcs.py
+++ funcs.py
@@ -1,35 +1,21 @@
 # this is where all the funcs needed for main.py will be
 
-ques_file=open("Questions\\question.txt")
-opt1_file=open("Questions\\option_1.txt")
-opt2_file=open("Questions\\option_2.txt")
-opt3_file=open("Questions\\option_3.txt")
-opt4_file=open("Questions\\option_4.txt")
-ans_file=open("Questions\\answer.txt")
-
-ques=ques_file.readlines()
-opt1=opt1_file.readlines()
-opt2=opt2_file.readlines()
-opt3=opt3_file.readlines()
-opt4=opt4_file.readlines()
-ans=ans_file.readlines()
-
-ques_file.close()
-opt1_file.close()
-opt2_file.close()
-opt3_file.close()
-opt4_file.close()
-ans_file.close()
-
 import random
+import csv
+
+ques_file=open("questions.csv","r")
+read_ques_file=csv.reader(ques_file)
+ques=[]         # 0-Q 1-O1 2-O2 3-O3 4-O4 5-A
+for x in read_ques_file:
+        ques.append(x)
 
 def quiz_again():      # Function To Start Another Quiz
         inp=input("Do you want to take another quiz? (Yes/No): ").upper()   # Ask If Want To Take One More Quiz Or Not
         if inp=="Y" or inp=="YES":
                 for ques_no in range(1,6):      # Number Of Questions To Be Asked In Each Quiz
                         r=random.randint(0,5)   # Random Questions From The Dicts (Also, Set The Range)
-                        ans1,ans2,ans3,ans4=opt1[r],opt2[r],opt3[r],opt4[r]     # Assigning Variables To All The Options For Each Iteration
-                        print(f"\nQuestion {str(ques_no)}: {ques[r]} 1. {opt1[r]} 2. {opt2[r]} 3. {opt3[r]} 4. {opt4[r]}")    # Displaying Question Along With Options
+                        ans1,ans2,ans3,ans4=ques[r][1],ques[r][2],ques[r][3],ques[r][4]     # Assigning Variables To All The Options For Each Iteration
+                        print(f"\nQuestion {str(ques_no)}: {ques[r][0]}\n 1. {ques[r][1]}\n 2. {ques[r][2]}\n 3. {ques[r][3]}\n 4. {ques[r][4]}")    # Displaying Question Along With Options
                         check_ans(answer="",r=r,ans1=ans1,ans2=ans2,ans3=ans3,ans4=ans4)  # Calling Function To Check The Answer
         elif inp=="N" or inp == "NO":
                 exit()  # FOR NOW Exit
@@ -38,9 +24,8 @@ def quiz_again():      # Function To Start Another Quiz
 
 def check_ans(answer,r,ans1,ans2,ans3,ans4):    # Function To Take And Check The Answer
         answer=int(input("\nEnter you answer (option 1, 2, 3, or 4): "))    # Input
-        if (answer==1 and ans1!=ans[r]) or (answer==2 and ans2!=ans[r])\
-                 or (answer==3 and ans3!=ans[r]) or (answer==4 and ans4!=ans[r]):       # If Answer Wrong
-                print(f"\nYour answer is incorrect.\nThe correct answer to this question is {ans[r]}")
+        if (answer==1 and ans1!=ques[r][5]) or (answer==2 and ans2!=ques[r][5]) or (answer==3 and ans3!=ques[r][5]) or (answer==4 and ans4!=ques[r][5]):       # If Answer Wrong
+                print(f"\nYour answer is incorrect.\nThe correct answer to this question is {ques[r][5]}")
         else:
                 print("\nYour answer is correct!\n")    # If Answer Right
 
