e02bd97e094f65c28e018d6352bce25a3dd53b68
Yugam4254 <yugamsehgal4254@gmail.com>
2021-09-11 15:03:50 +0530

    Initial Commit

--- funcs.py
+++ funcs.py
@@ -1,14 +1,13 @@
-# this is where all the functions will be that are needed for main.py
+# this is where all the funcs needed for main.py will be
 
-# function to print all the questions and get answer
-
-def print_ques():
-        with open("Questions/question.txt") as f:
-                ques = f.readlines()
-                for ques_no in range(6):
-                        print(f"Q{int(ques_no)+1}: {ques[ques_no]}")
-
-print_ques()
+#These ques ans are here to test the fucntions
+ques={   1:"Which is the smallest planet within our solar system?"
+        ,2:"Which is the second smallest planet within our solar system?"
+        ,3:"The moon called Titan orbits which planet?"
+        ,4:"Which is the brightest planet in the night’s sky?"
+        ,5:"Which is the largest planet within our solar system?"
+        ,6:"Uranus has only been visited by what spacecraft?"
+}
 
 opt1={   1:"Mercury"                            #C
         ,2:"Neptune"
@@ -54,30 +53,28 @@ import random
 
 def quiz_again():      # Function To Start Another Quiz
         inp=input("Do you want to take another quiz? (Yes/No): ").upper()   # Ask If Want To Take One More Quiz Or Not
-        if inp=="Y" or "YES":
+        if inp=="Y" and "YES":
                 for ques_no in range(1,6):      # Number Of Questions To Be Asked In Each Quiz
                         r=random.randint(1,6)   # Random Questions From The Dicts (Also, Set The Range)
                         ans1,ans2,ans3,ans4=opt1[r],opt2[r],opt3[r],opt4[r]     # Assigning Variables To All The Options For Each Iteration
                         print(f"\nQuestion {str(ques_no)}: {ques[r]}.\n1. {opt1[r]}\
                                 \n2. {opt2[r]}\n3. {opt3[r]}\n4. {opt4[r]}")    # Displaying Question Along With Options
                         check_ans(answer="",r=r,ans1=ans1,ans2=ans2,ans3=ans3,ans4=ans4)  # Calling Function To Check The Answer
-        else:
+        elif inp=="N" and "NO":
                 exit()  # FOR NOW Exit
+        else:
+                exit()  # FOR NOW Exit 
 
 def check_ans(answer,r,ans1,ans2,ans3,ans4):    # Function To Take And Check The Answer
-        answer=input("\nEnter you answer (option 1, 2, 3, or 4): ")     # Input
-        if answer!=None:        # Checking The Answer
-                if answer=="1" and ans1==ans[r]:        # If Selected 1
-                        print("\nYour answer is correct!\n")
-                elif answer=="2" and ans2==ans[r]:      # If Selected 2
-                        print("\nYour answer is correct!\n")
-                elif answer=="3" and ans3==ans[r]:      # If Selected 3
-                        print("\nYour answer is correct!\n")
-                elif answer=="4" and ans4==ans[r]:      # If Selected 4
-                        print("\nYour answer is correct!\n")
-                else:
-                        print(f"\nYour answer is incorrect.\nThe correct answer to this question is {ans[r]}.\n")
-        else:
-                pass
+        answer=int(input("\nEnter you answer (option 1, 2, 3, or 4): "))    # Input
+        
+        for x in range(1):        # Checking The Answer
+                print("\nYour answer is correct!\n" if answer==1 and ans1==ans[r] else "", end="")      # If Selected 1
+                print("\nYour answer is correct!\n" if answer==2 and ans2==ans[r] else "", end="")      # If Selected 2
+                print("\nYour answer is correct!\n" if answer==3 and ans3==ans[r] else "", end="")      # If Selected 3
+                print("\nYour answer is correct!\n" if answer==4 and ans4==ans[r] else "", end="")      # If Selected 4
+                if (answer==1 and ans1!=ans[r]) or (answer==2 and ans2!=ans[r])\
+                 or (answer==3 and ans3!=ans[r]) or (answer==4 and ans4!=ans[r]):
+                        print(f"\nYour answer is incorrect.\nThe correct answer to this question is {ans[r]}.") # If Answer Wrong
 
 quiz_again()
