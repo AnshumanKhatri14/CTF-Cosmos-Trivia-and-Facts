faab282e3758d4d6713d3bdf6f356317f6963995
AnshumanKhatri14 <anshumankhatri2004@gmail.com>
2021-09-12 11:29:48 +0530

    more commit

--- funcs.py
+++ funcs.py
@@ -32,3 +32,21 @@ def check_ans(answer,r,ans1,ans2,ans3,ans4):    # Function To Take And Check The
         print("\nYour answer is correct!")    # If Answer Right
 
 quiz_again()
+
+import random
+def facts():                         #Defining facts function
+    inpt=input("Would you like to know a fun fact about Space? (y/n) : ").lower()       #Asking user would they like to know a space fact
+    if inpt=="y":                                                                       #When agreed
+        import random 
+        with open("babatunde.txt","r") as f:
+            facts=f.readlines()
+            print("Your random space fact is : " )
+            print(random.choice(facts))
+    elif inpt=="n":                                                                     #When denied
+        print("Hope you try it later :)")
+        exit()
+    else:                                                                               #When input invalid
+        print("Your input is invalid, try again :)")
+        exit()
+
+facts()                                                                                 #Calling function
